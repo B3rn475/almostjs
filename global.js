3ed96b010335481ac6fe57a0433ffecb1d40c83c
Carlo Bernaschina <b3rn4@hotmail.it>
2017-02-03 15:03:45 +0100

    Added browser registration

---
+++ global.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, the ALMOsT project authors. Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by a MIT-style license that can be found in the LICENSE file.
+/*jslint node: true*/
+"use strict";
+
+var window = require('window');
+
+if (window.almost) {
+    throw new Error('ALMOsT should be loaded before any plugin');
+}
+window.almost = require('./');
+window.almost.plugins = {};
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+/*jslint node: true, nomen: true */
+"use strict";
+
+var gulp = require('gulp'),
+    rimraf = require('gulp-rimraf'),
+    rename = require('gulp-rename'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    extractor = require('gulp-extract-sourcemap'),
+    minifyjs = require('gulp-uglify');
+
+gulp.task('clean', function () {
+    return gulp.src('./dist/*', {read: false, dot: true}).pipe(rimraf({ force: true }));
+});
+
+var expose = {
+    'window': 'window',
+};
+
+gulp.task('almost.js', function () {
+    return browserify({
+        entries: './global.js',
+        debug: true,
+    })
+        .transform('exposify', {
+            expose: expose
+        })
+        .bundle()
+        .pipe(source('almost.js'))
+        .pipe(buffer())
+        .pipe(extractor({
+            basedir: __dirname,
+            fakeFix: true
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('almost.min.js', function () {
+    return browserify({
+        entries: './global.js',
+        debug: false,
+    })
+        .transform('exposify', {
+            expose: expose
+        })
+        .bundle()
+        .pipe(source('almost.min.js'))
+        .pipe(buffer())
+        .pipe(minifyjs())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['almost.js', 'almost.min.js']);
+
+gulp.task('default', ['clean'], function () {
+    return gulp.start('build');
+});
